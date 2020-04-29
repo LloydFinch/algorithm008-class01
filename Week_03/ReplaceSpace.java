4becb19fb78c54abaa7aa01837fe2f2d3f7968a3
LloydFinch <wangwenqi@orangelab.cn>
2020-04-29 15:22:34 +0800

    add replace code

---
+++ Week_03/ReplaceSpace.java
@@ -0,0 +1,17 @@
+package Week_03;
+
+public class ReplaceSpace {
+    public String replaceSpace(String s) {
+        StringBuilder sb = new StringBuilder();
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == ' ') {
+                sb.append("%20");
+            } else {
+                sb.append(chars[i]);
+            }
+        }
+
+        return sb.toString();
+    }
+}
